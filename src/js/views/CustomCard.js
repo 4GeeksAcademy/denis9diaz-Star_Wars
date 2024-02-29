5979254df9e0ab5bc8aecc1e87c4d3ec9d8672bc
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-02-29 13:17:00 +0000

    botones

--- src/js/views/CustomCard.js
+++ src/js/views/CustomCard.js
@@ -11,9 +11,18 @@ const CustomCard = (props) => {
                 <h5 className="card-title">{props.name}</h5>
                 <div className="d-flex justify-content-between">
                     <Link to={`/individual/${props.id}`}>
-                        <button className="btn btn-primary">Learn more</button>
+                        <button className="custom-btn btn-1">Read More</button>
                     </Link>
-                    <button className="btn btn-outline-warning align-self-end"><i className="fa-regular fa-heart"></i></button>
+                    <button className="bookmarkBtn align-self-end">
+                        <span className="IconContainer">
+                            <svg viewBox="0 0 384 512" height="0.9em" class="icon">
+                                <path
+                                    d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"
+                                ></path>
+                            </svg>
+                        </span>
+                        <p className="text"></p>
+                    </button>
                 </div>
             </div>
         </div>
